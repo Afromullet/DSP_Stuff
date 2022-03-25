2fd271c3deb06735ef3f4cfa17505587a6406991
Afromullet <seankharris91@gmail.com>
2022-03-25 16:16:18 -0400

    Added more Python prototypes
    Added more Python prototypes

--- python-signal-test.py
+++ python-signal-test.py
@@ -49,16 +49,12 @@ def example_ppm_using_samples():
     ax.plot(y)
     
 def example_ppm():
-
-
-
-
-
+    
     freq_1 = 3
     freq_2 = 7
     fs = 176000
     T = 1 / fs
-        
+    
     #Creating a pwm signal
     x = np.arange(0,2,T)
     x_split = np.array_split(x,2)
@@ -66,7 +62,7 @@ def example_ppm():
     msg = np.sin(2*np.pi * 5 *x)
         
     
-    saw_freq_1 = signal.sawtooth(2 * np.pi * freq_1 * x_split[0])
+    saw_freq_1 = np.sin(2 * np.pi * freq_1 * x_split[0])
     saw_freq_2 = signal.sawtooth(2 * np.pi * freq_2 * x_split[1])
     combined_saw = np.append(saw_freq_1,saw_freq_2)
     
@@ -84,16 +80,66 @@ def example_ppm():
             k=k+50;
         else: 
             k=k+1
+    x_half_size = len(x)//2
+    fig,ax = plt.subplots(4)
+    ax[0].plot(x,msg)
+    ax[1].plot(combined_saw)
+    ax[2].plot(pwm)
+    ax[3].plot(ppm)
     
-    
-    
-    fig,ax = plt.subplots(5)
-    ax[0].plot(msg)
-    ax[1].plot(saw_freq_1)
-    ax[2].plot(saw_freq_2)
-    ax[3].plot(pwm)
-    ax[4].plot(ppm)
 
-#example_ppm_using_samples()
-#example_pwm()
-example_ppm()
+def amp_shift_key():
+
+    fs = 44100  # sampling rate
+    baud = 300  # symbol rate
+    Nbits = 10  # number of bits
+    Ns = int(fs/baud)
+    N = Nbits * Ns
+    f0 = 1800
+    bits = np.random.randn(Nbits,1) > 0
+    np.random.seed(seed=1)
+    bits = np.random.randn(Nbits,1) > 0
+    M = np.tile(bits,(1,Ns))
+    t = np.r_[0.0:N]/fs
+    OOK = M.ravel()*np.sin(2*np.pi*f0*t)
+    
+    fig = plt.figure(figsize = (16,4))
+    plt.plot(t,OOK)
+    plt.xlabel('time [s]')    
+
+
+
+#Using nested functions to keep things clean for now before deciding on a final method
+def ppm_method_2():
+
+
+    def rect(T):
+        """create a centered rectangular pulse of width $T"""
+        return lambda t: (-T/2 <= t) & (t < T/2)
+    
+
+    
+
+    def pulse_train(t, at, shape):
+        """create a train of pulses over $t at times $at and shape $shape"""
+        return np.sum(shape(t - at[:,np.newaxis]), axis=0)
+
+    sig = pulse_train(
+        t=np.arange(100),              # time domain
+        at=np.array([0, 20, 40, 80]),  # times of pulses
+        shape=rect(10)                 # shape of pulse
+    )
+
+    fs = 176000
+    T = 1 / fs
+    sig2 = pulse_train(
+        t=np.arange(0,80,T),              # time domain
+        at=np.array([0, 5, 3, 80]),  # times of pulses
+        shape=rect(1)                 # shape of pulse
+    )
+    
+    fig,ax = plt.subplots()
+    ax.plot(sig2)
+    
+
+ppm_method_2()
