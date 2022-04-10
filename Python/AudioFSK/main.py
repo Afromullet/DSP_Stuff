5492b288527277bb9462bf608a20d7ed8662f470
Afromullet <seankharris91@gmail.com>
2022-04-10 10:02:15 -0400

    Selecting random octaves for message generation
    Selecting random octaves for message generation

--- Python/AudioFSK/main.py
+++ Python/AudioFSK/main.py
@@ -10,7 +10,7 @@ from scipy.io.wavfile import write
 from collections import namedtuple
 from csv import DictReader
 import wave
-
+import random
 
 '''
 Stores the samples of a note.
@@ -40,7 +40,7 @@ def read_ascii_table():
     return ascii_mappings
 
 
-def get_wave(freq,fs=44100,duration=0.5):
+def get_wave(freq,fs=44100,duration=0.1):
     '''
     Self explanatory
     '''
@@ -50,15 +50,13 @@ def get_wave(freq,fs=44100,duration=0.5):
     
     return wave
 
-def get_piano_notes():
+def get_piano_notes(base_freq):
     '''
     Returns a dict object for all the piano 
     note's frequencies
     '''
     # White keys are in Uppercase and black keys (sharps) are in lowercase
     octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B'] 
-    base_freq = 261.63 #Frequency of Note C4
-    
     note_freqs = {octave[i]: base_freq * pow(2,(i/12)) for i in range(len(octave))}        
     note_freqs[''] = 0.0 # silent note
     
@@ -76,7 +74,7 @@ def convert_letter_to_notes(letter,symbol_list,a_mappings,bits_per_symbol):
     '''
     Returns the notes required for a letter
     I.E, Binary representation of A = 01000001
-    A note is mapped to a symbol..i.i CSharp = 00 and an ASharp = 11
+    A note is mapped to a symbol..i.i FSharp = 00 and an A = 11
     Returns the note and the samples associated with that note
     We're using a generator because we don't want to return a ridiculous number of samples at once
     '''
@@ -89,11 +87,11 @@ def convert_letter_to_notes(letter,symbol_list,a_mappings,bits_per_symbol):
             symbol = [value.note for value in symbol_list if value.bit == binary[i:i+bits_per_symbol]] 
             yield symbol[0]
      
-def write_message_to_wav(msg,a_table,symbols,fname,bits_per_symbol,fs=44100):
+def write_message_to_wav(msg,a_table,symbols,fname,bits_per_symbol,bytes_per_sample,fs=44100):
     
     f = wave.open(fname + ".wav", "w")
     f.setnchannels(1)
-    f.setsampwidth(2)   # 2 bytes per sample.
+    f.setsampwidth(bytes_per_sample)   # 2 bytes per sample.
     f.setframerate(fs)
     for letter in msg:
        
@@ -103,26 +101,89 @@ def write_message_to_wav(msg,a_table,symbols,fname,bits_per_symbol,fs=44100):
             audio = val.samples * (16300/np.max(val.samples)) # Adjusting the Amplitude     
             f.writeframes(audio.astype(np.int16).tobytes())
     f.close()
-            
+    
+    
+def read_message_from_wav(fname):
+    
+    obj = wave.open(fname + ".wav",'r')
+    print( "Number of channels",obj.getnchannels())
+    print ( "Sample width",obj.getsampwidth())
+    print ( "Frame rate.",obj.getframerate())
+    print ("Number of frames",obj.getnframes())
+    print ( "parameters:",obj.getparams())
+    a = obj.getnframes()
+    print(a)
+    obj.close()
+    
+    
+def basic_message_example():
+    C4_freq = 261.63
+    B4_freq = 493.88
+    
+    fname = "testfile1"
+    a_table = read_ascii_table()
+    message = "I cast a fireball"
+    C4_Octave = get_piano_notes(C4_freq)
+    B4_Octave = get_piano_notes(B4_freq)
+    
+
+    symbols = [
+    Symbol( Note("A",get_wave(C4_Octave['A'])),"0000"),
+    Symbol( Note("B",get_wave(C4_Octave['B'])),"0001"),
+    Symbol( Note("C",get_wave(C4_Octave['C'])),"0010"),
+    Symbol( Note("D",get_wave(C4_Octave['D'])),"0011"),
+    Symbol( Note("A",get_wave(B4_Octave['A'])),"0100"),
+    Symbol( Note("B",get_wave(B4_Octave['B'])),"0101"),
+    Symbol( Note("C",get_wave(B4_Octave['C'])),"0110"),
+    Symbol( Note("D",get_wave(B4_Octave['D'])),"0111"),
+    Symbol( Note("E",get_wave(C4_Octave['E'])),"1000"),
+    Symbol( Note("F",get_wave(C4_Octave['F'])),"1001"),
+    Symbol( Note("G",get_wave(C4_Octave['G'])),"1010"),
+    Symbol( Note("E",get_wave(B4_Octave['E'])),"1011"),
+    Symbol( Note("F",get_wave(B4_Octave['F'])),"1100"),
+    Symbol( Note("G",get_wave(B4_Octave['G'])),"1101"),
+    Symbol( Note("a",get_wave(C4_Octave['a'])),"1110"),
+    Symbol( Note("a",get_wave(B4_Octave['a'])),"1111")
+    ]
+    
+    write_message_to_wav(message,a_table,symbols,fname,4,2)
+    read_message_from_wav(fname)
+    
+    
+def create_random_symbol_groups(base_freq_1,base_freq_2):
+    
+    '''
+    Uses 4 symbols per bit. Breaks down the symbols into two groups, each associated with a different octave
+    Selects a random note from that octave and maps it to a symbol.
+    Returns the symbol list
+    '''
+    symbol_sets = [["0000","0001","0010","0011","0100", "0101","0110","0111"],[ "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"]]
+    octave = ['C', 'c', 'D', 'd', 'E', 'F', 'f', 'G', 'g', 'A', 'a', 'B'] 
+    note_frequencies = [get_piano_notes(base_freq_1),get_piano_notes(base_freq_2)] #List of lists, where each sublist are all the octaves of a base frequency
+
+    symbols = [] 
+    for i,symbol_set in enumerate(symbol_sets):
+        #Creates a list of random indices for a symbol set. The index will be used to select a random note. 
+        random_indices = random.sample(range(len(symbol_set)),len(symbol_set))
+        for j,rand_num in enumerate(random_indices):
+            random_octave = octave[rand_num]
+            current_symbol = symbol_set[j]
+            symbol =  Symbol(
+                Note(random_octave,get_wave(note_frequencies[i][random_octave])),
+                symbol_set[j])
+            symbols.append(symbol)
+       
+    return symbols
+    
 '''
 Test data
 
 '''
+
+symbols = create_random_symbol_groups(261.63,493.88)
+
+fname = "testfile1"
 a_table = read_ascii_table()
-message = "The The The in a thing a the"
-all_notes = get_piano_notes()
-
-Note_C = Note("C",get_wave(all_notes['C']))
-Note_A = Note("A",get_wave(all_notes['A']))
-Note_D = Note("D",get_wave(all_notes['D']))
-Note_E = Note("E",get_wave(all_notes['E']))
-
-Symbol_0 = Symbol(Note_C,"00")
-Symbol_1 = Symbol(Note_A,"10")
-Symbol_2 = Symbol(Note_D,"01")
-Symbol_3 = Symbol(Note_E,"11")
-
-symbols = [Symbol_0,Symbol_1,Symbol_2,Symbol_3]
-
-
-write_message_to_wav(message,a_table,symbols,"testfile",2)
+message = "I cast a fireball"
+write_message_to_wav(message,a_table,symbols,fname,bits_per_symbol=4,bytes_per_sample=2)
+#basic_message_example()
