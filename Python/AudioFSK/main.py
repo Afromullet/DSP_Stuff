9e034bebfebb6ba45e7f99a89ab0674c6407fa90
Afromullet <seankharris91@gmail.com>
2022-05-13 16:23:12 -0400

    Fixed a bug in the way symbols were read
    Didn't process the symbol list correctly, which resulted in too many frequencies for a letter

--- Python/AudioFSK/main.py
+++ Python/AudioFSK/main.py
@@ -11,9 +11,11 @@ from csv import DictReader
 import wave
 import random
 import struct
+import scipy
 from scipy import signal
 import matplotlib.pyplot as plt
-from scipy.signal import butter, sosfilt, sosfreqz
+from scipy.signal import butter, sosfilt
+from itertools import zip_longest
 
 
 '''
@@ -37,6 +39,14 @@ Knowing the symbol duration lets us know how large our FFT slices should be
 Encoding_Params = namedtuple("Encoding_Params", "fs symbol_duration")
 
 
+
+def grouper(iterable, n, fillvalue=None):
+    '''
+    grouper('ABCDEFG', 3, 'x')  # --> 'ABC' 'DEF' 'Gxx'
+
+    '''
+    args = [iter(iterable)] * n
+    return zip_longest(*args, fillvalue=fillvalue)
 
 def read_ascii_table():
     '''
@@ -79,6 +89,20 @@ def create_binary_from_ascii_letter(letter,mapping_table):
     return [value.binary for value in mapping_table if value.letter == letter]  
 
 
+def get_ascii_from_binary(binary,mapping_table):
+    '''
+    Gets the ascii letter of a byte
+    '''
+    
+    for value in mapping_table:
+        
+        if value.binary == binary:
+            return value
+
+        
+ 
+
+
 def convert_letter_to_notes(letter,symbol_list,a_mappings,bits_per_symbol):
     '''
     Returns the notes required for a letter
@@ -90,11 +114,21 @@ def convert_letter_to_notes(letter,symbol_list,a_mappings,bits_per_symbol):
     binary = create_binary_from_ascii_letter(letter,a_mappings)
 
     if len(binary):
+        
+        
+        #Doing some extra work now by combining the string, then splitting it into chunks. Those steps can be combined in the future once things work todo
+       
         binary = ''.join((map(str,binary))) #Originally a list and we want a string..Todo just use binary[0] in the future
-  
-        for i in range(len(binary)-(bits_per_symbol-1)):    
-            symbol = [value.note for value in symbol_list if value.bit == binary[i:i+bits_per_symbol]] 
+        binary = grouper(binary,bits_per_symbol)
+        
+        for sym in binary:
+            
+            #Here is the extra work part where we combine the chunks and the part we have to combine in the future todo
+            sym = ''.join((map(str,sym))) 
+            symbol = [value.note for value in symbol_list if value.bit == sym]
             yield symbol[0]
+     
+   
      
 def write_message_to_wav(msg,a_table,symbols,fname,bits_per_symbol,bytes_per_sample,fs=44100):
     
@@ -115,12 +149,6 @@ def write_message_to_wav(msg,a_table,symbols,fname,bits_per_symbol,bytes_per_sam
 def read_message_from_wav(fname):
     
     obj = wave.open(fname + ".wav",'r')
-    # print( "Number of channels",obj.getnchannels())
-    # print ( "Sample width",obj.getsampwidth())
-    # print ( "Frame rate.",obj.getframerate())
-    # print ("Number of frames",obj.getnframes())
-    # print ( "parameters:",obj.getparams())
-    
     frames = []
     num_frames = obj.getnframes()
     for frame in range(num_frames):
@@ -131,39 +159,35 @@ def read_message_from_wav(fname):
     
     
 def basic_message_example(fname,message):
-    C4_freq = 261.63
-    B4_freq = 493.88
+    octave_1_freq = 161.63
+    ocatave_2_freq = 593.88
     
- 
     a_table = read_ascii_table()
  
-    C4_Octave = get_piano_notes(C4_freq)
-    B4_Octave = get_piano_notes(B4_freq)
+    octave_1 = get_piano_notes(octave_1_freq)
+    octave_2 = get_piano_notes(ocatave_2_freq)
     
-    
-    #todo get min and max freq of both octaves
-    
+    #todo get min and max freq of both octaves think I already did this verify later 
     encoding_params = Encoding_Params((44100), 0.1)
-    freq_ranges = [get_symbol_freq_range(C4_Octave),get_symbol_freq_range(B4_Octave)]
-    
+    freq_ranges = [get_symbol_freq_range(octave_1),get_symbol_freq_range(octave_2)]
     
     symbols = [
-    Symbol( Note("A",get_wave(C4_Octave['A'],encoding_params.fs,encoding_params.symbol_duration)),"0000",C4_Octave['A']),
-    Symbol( Note("B",get_wave(C4_Octave['B'],encoding_params.fs,encoding_params.symbol_duration)),"0001",C4_Octave['B']),
-    Symbol( Note("C",get_wave(C4_Octave['C'],encoding_params.fs,encoding_params.symbol_duration)),"0010",C4_Octave['C']),
-    Symbol( Note("D",get_wave(C4_Octave['D'],encoding_params.fs,encoding_params.symbol_duration)),"0011",C4_Octave['D']),
-    Symbol( Note("A",get_wave(B4_Octave['A'],encoding_params.fs,encoding_params.symbol_duration)),"0100",B4_Octave['A']),
-    Symbol( Note("B",get_wave(B4_Octave['B'],encoding_params.fs,encoding_params.symbol_duration)),"0101",B4_Octave['B']),
-    Symbol( Note("C",get_wave(B4_Octave['C'],encoding_params.fs,encoding_params.symbol_duration)),"0110",B4_Octave['C']),
-    Symbol( Note("D",get_wave(B4_Octave['D'],encoding_params.fs,encoding_params.symbol_duration)),"0111",B4_Octave['D']),
-    Symbol( Note("E",get_wave(C4_Octave['E'],encoding_params.fs,encoding_params.symbol_duration)),"1000",C4_Octave['E']),
-    Symbol( Note("F",get_wave(C4_Octave['F'],encoding_params.fs,encoding_params.symbol_duration)),"1001",C4_Octave['F']),
-    Symbol( Note("G",get_wave(C4_Octave['G'],encoding_params.fs,encoding_params.symbol_duration)),"1010",C4_Octave['G']),
-    Symbol( Note("E",get_wave(B4_Octave['E'],encoding_params.fs,encoding_params.symbol_duration)),"1011",B4_Octave['E']),
-    Symbol( Note("F",get_wave(B4_Octave['F'],encoding_params.fs,encoding_params.symbol_duration)),"1100",B4_Octave['F']),
-    Symbol( Note("G",get_wave(B4_Octave['G'],encoding_params.fs,encoding_params.symbol_duration)),"1101",B4_Octave['G']),
-    Symbol( Note("a",get_wave(C4_Octave['a'],encoding_params.fs,encoding_params.symbol_duration)),"1110",C4_Octave['a']),
-    Symbol( Note("a",get_wave(B4_Octave['a'])),"1111",B4_Octave['a'])
+    Symbol( Note("A",get_wave(octave_1['A'],encoding_params.fs,encoding_params.symbol_duration)),"0000",octave_1['A']),
+    Symbol( Note("B",get_wave(octave_1['B'],encoding_params.fs,encoding_params.symbol_duration)),"0001",octave_1['B']),
+    Symbol( Note("C",get_wave(octave_1['C'],encoding_params.fs,encoding_params.symbol_duration)),"0010",octave_1['C']),
+    Symbol( Note("D",get_wave(octave_1['D'],encoding_params.fs,encoding_params.symbol_duration)),"0011",octave_1['D']),
+    Symbol( Note("A",get_wave(octave_2['A'],encoding_params.fs,encoding_params.symbol_duration)),"0100",octave_2['A']),
+    Symbol( Note("B",get_wave(octave_2['B'],encoding_params.fs,encoding_params.symbol_duration)),"0101",octave_2['B']),
+    Symbol( Note("C",get_wave(octave_2['C'],encoding_params.fs,encoding_params.symbol_duration)),"0110",octave_2['C']),
+    Symbol( Note("D",get_wave(octave_2['D'],encoding_params.fs,encoding_params.symbol_duration)),"0111",octave_2['D']),
+    Symbol( Note("E",get_wave(octave_1['E'],encoding_params.fs,encoding_params.symbol_duration)),"1000",octave_1['E']),
+    Symbol( Note("F",get_wave(octave_1['F'],encoding_params.fs,encoding_params.symbol_duration)),"1001",octave_1['F']),
+    Symbol( Note("G",get_wave(octave_1['G'],encoding_params.fs,encoding_params.symbol_duration)),"1010",octave_1['G']),
+    Symbol( Note("E",get_wave(octave_2['E'],encoding_params.fs,encoding_params.symbol_duration)),"1011",octave_2['E']),
+    Symbol( Note("F",get_wave(octave_2['F'],encoding_params.fs,encoding_params.symbol_duration)),"1100",octave_2['F']),
+    Symbol( Note("G",get_wave(octave_2['G'],encoding_params.fs,encoding_params.symbol_duration)),"1101",octave_2['G']),
+    Symbol( Note("a",get_wave(octave_1['a'],encoding_params.fs,encoding_params.symbol_duration)),"1110",octave_1['a']),
+    Symbol( Note("a",get_wave(octave_2['a'])),"1111",octave_2['a'])
     ]
     
     write_message_to_wav(message,a_table,symbols,fname,4,2)
@@ -220,6 +244,47 @@ def butter_bandpass_filter(data, lowcut, highcut, fs, order=5):
         y = sosfilt(sos, data)
         return y
     
+    
+def get_peaks(column):
+    '''
+    Simple prototype function that finds the peaks. Will expand in the future todo
+    '''
+    height_threshold=1000 # We need a threshold. 
+    peaks_index, properties = signal.find_peaks(np.abs(column), height=height_threshold)
+    return peaks_index
+
+def get_frequencies(Zxx,freqs):
+    
+    '''
+    Gets the frequencies of teh peaks. Not using any vectorization at the moment so not applicable to large sffts
+    Once the initial decoding works, we'll optimize it. 
+    
+    '''
+    frequencies = np.array([])
+
+    for i in range(len(Zxx[0,:])):
+        peaks_index = get_peaks(Zxx[:,i])
+        frequencies =  np.append(frequencies,freqs[peaks_index])  
+    return frequencies
+
+
+def plot_stft(t,f,Zxx):
+    
+    fig,ax = plt.subplots()
+    plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=2500, shading='gouraud')
+    ax.set_ylim(0,2500)
+    plt.title('STFT abc')
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    plt.show()
+    
+def __get_all_freqs_from_symbols__(symbols):
+    '''
+    Debug function. Gets all the frequencies of the symbols
+    '''
+    return [sym.freq for sym in symbols]
+
+    
 '''
 Test data
 
@@ -228,25 +293,62 @@ Test data
 #symbols = create_random_symbol_groups(261.63,493.88)
 #write_message_to_wav(message,a_table,symbols,fname,bits_per_symbol=4,bytes_per_sample=2)
 
-fname = "testfile1"
+fname = "testfile2"
 a_table = read_ascii_table()
-message = "cba"
+message = "a"
 
+
+binary = [create_binary_from_ascii_letter(letter,a_table) for letter in message]
 
 symbols,encoding_params,freq_ranges = basic_message_example(fname,message)
-frames = read_message_from_wav("testfile1")
-
-octave1_filtered = butter_bandpass_filter(frames,freq_ranges[0]["low"],freq_ranges[0]["high"],encoding_params.fs)
-octave2_filtered = butter_bandpass_filter(frames,freq_ranges[1]["low"],freq_ranges[1]["high"],encoding_params.fs)
-frames = octave1_filtered + octave2_filtered
+frames = read_message_from_wav(fname)
 
 f, t, Zxx = signal.stft(frames, encoding_params.fs, nperseg=2056)
+plot_stft(t,f,Zxx)
 
-
-fig,ax = plt.subplots()
-plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=2500, shading='gouraud')
-ax.set_ylim(0,2500)
-plt.title('STFT lol wtf')
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
+N = 600
+yf = scipy.fft(frames)
+T = 1.0 / 800.0
+xf = np.linspace(0.0, 1.0/(2.0*T), N//2)
+fig, ax = plt.subplots()
+ax.plot(xf, 2.0/N * np.abs(yf[:N//2]))
 plt.show()
+
+
+freqs = get_frequencies(Zxx,f)
+
+pairs = []
+decoded_symbols = []
+for i in range(0,len(freqs) - 1,2):
+  
+    symbol_pair = [0,0]
+    for sym in symbols:
+        
+        freq1_diff = np.abs(sym.freq-freqs[i])
+        freq2_diff = np.abs(sym.freq-freqs[i+1])
+        print(freq2_diff)
+        if freq1_diff >= 0 and freq1_diff <= 1:
+            symbol_pair[0] = sym.bit
+          
+        if freq2_diff >= 0 and freq2_diff <= 1:
+            symbol_pair[1] = sym.bit       
+            
+            
+        if symbol_pair[0] != 0 and symbol_pair[1] != 0:
+            decoded_symbols.append(symbol_pair[0] + symbol_pair[1])
+            break
+            
+        
+        
+letters = []
+
+print("start")
+for binary in decoded_symbols:
+    print(binary)
+    letters.append(get_ascii_from_binary(binary,a_table)) 
+       
+
+    
+sym_freqs = __get_all_freqs_from_symbols__(symbols)
+    
+
